daf1343b26a9bd8663407f4a100f8a2b2d273353
Vittorione <vittorio.tiozzo.dev@gmail.com>
2024-09-29 01:41:16 +0200

    Task: main update

--- main.py
+++ main.py
@@ -21,14 +21,21 @@ class Shape:
 
 
 class Circle(Shape):
-    def draw(self):
-        circle = plt.Circle((0.5, 0.5), self.number)
-        fig, ax = plt.subplots()
-        ax.axis("equal")
-        ax.set_xlim([(self.number * (-1)), self.number])
-        ax.set_ylim([(self.number * (-1)), self.number])
+    def __init__(self, r):
+        super().__init__(r)
+        self.r = r
 
-        ax.add_patch(circle)
+    def draw(self):
+        t = np.linspace(0, 2 * np.pi, 400)
+        x = self.r * np.cos(t)
+        y = self.r * np.sin(t)
+        plt.plot(x, y, label=f'Circonferenza: $r = {self.r}$')
+        plt.gca().set_aspect('equal')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title('Grafico di una circonferenza')
+        plt.legend()
+        plt.grid(True)
         plt.show()
 
     def calculate_area(self):
@@ -39,10 +46,22 @@ class Circle(Shape):
 
 
 class Parable(Shape):
+    def __init__(self, a, b, c):
+        super().__init__(a)
+        self.b = b
+        self.c = c
+
+    def get_b(self):
+        return self.b
+
+    def get_c(self):
+        return self.c
+
     def draw(self):
-        a = 1
-        b = 0
-        c = 0
+
+        a = self.number
+        b = self.b
+        c = self.c
 
         x = np.linspace(-10, 10, 400)
         y = a * pow(x, 2) + b * x + c
@@ -58,28 +77,42 @@ class Parable(Shape):
 
 
 class Ellipse(Shape):
-    def __init__(self, number, number2):
-        super().__init__(number2)
-        self.number2 = number2
+    def __init__(self, a, b):
+        super().__init__(a)
+        self.b = b
+        self.a = self.number
 
-    def get_number2(self):
-        return self.number2
+    def get_b(self):
+        return self.b
 
     def draw(self):
-        pass
+        t = np.linspace(0, 2 * np.pi, 400)
+
+        x = self.a * np.cos(t)
+        y = self.b * np.sin(t)
+        plt.plot(x, y, label=f'Ellisse: $x^2/{self.a^2} + y^2/{self.b^2} = 1$')
+        plt.gca().set_aspect('equal')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title('Grafico di un ellisse')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
 
     def calculate_area(self):
-        return 3.14 * self.number * self.number2
+        return 3.14 * self.a * self.b
 
     def calculate_perimeter(self):
-        return 2 * 3.14 * math.sqrt((pow(self.number, 2) + pow(self.number2, 2)) / 2.0)
+        return 2 * 3.14 * math.sqrt((pow(self.a, 2) + pow(self.b, 2)) / 2.0)
 
 
 def main():
     c = Circle(5)
-    d = Parable(2)
+    d = Parable(1, 5, 2)
+    s = Ellipse(5, 3)
     print(c.draw())
-    print(c.draw(d.draw()))
+    print(d.draw())
+    print(s.draw())
 
 
 if __name__ == '__main__':
